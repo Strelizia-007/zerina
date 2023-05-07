514b8d6ff9e6364e4e8fcd679a5387a5f82f711e
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-05-07 19:13:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,7 +74,7 @@ TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 COMMAND_HAND_LER = ("/")
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Zeherin:<password>@cluster0.2t0eici.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Zeherin:Zeherin@cluster0.2t0eici.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Zerina")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 MONGO_URL = os.environ.get('MONGO_URL', "mongodb+srv://Zeherin:<password>@cluster0.2t0eici.mongodb.net/?retryWrites=true&w=majority")
