8bb3dd593c17adc5e68bb610dacd1f04477aa8f8
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-08-07 21:24:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,7 +75,7 @@ COMMAND_HAND_LER = ("/")
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Zeherin:Zeherin@cluster0.2t0eici.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Zerina")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Zeherin")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 MONGO_URL = os.environ.get('MONGO_URL', "mongodb+srv://Zeherin:Zeherin@cluster0.2t0eici.mongodb.net/?retryWrites=true&w=majority")
 
